31304b540623fb0b4ccd81a08a8aee4d72787fac
hamidt.com@gmail.com <hamidt.com@gmail.com>
2018-11-16 01:31:59 +0330

    edit added, tags color added, bootstrap added, some changes in code structrue

--- source/index.js
+++ source/index.js
@@ -4,30 +4,66 @@ import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 
 import './style/style.scss';
 
-var SimpleListModel = function(items) {
-  let tagsList = [];
-this.items = ko.observableArray(items);
-this.itemToAdd = ko.observable("");
-this.remove = function(val){
-this.items.splice(this.items.indexOf(val), 1)
-}.bind(this);
-
-this.isNumber = function(value){
-if(!isNaN(value) && value !== '') return true;
-return false; 
-}.bind(this);
-
-this.addItem = function() {
-  let value = this.itemToAdd();
-  let normalizedValue = value.replace(/;|\r?\n|\r/g, ",").split(',');
-  for(let i=0; normalizedValue.length > i; i++){
-    //filter for adding unique tags and numbers
-    if(this.items().indexOf(normalizedValue[i])  === -1 && this.isNumber(normalizedValue[i])){
-      this.items.push(normalizedValue[i]);
-    }
+class TagInput{
+  constructor(items){
+    this.items = ko.observableArray(items);
+    this.tagsList = ko.observable(this.items().join(','));
+    this.itemToAdd = ko.observable("");
+    this.remove = this.remove.bind(this);
+    this.updateTagsList = this.updateTagsList.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.editTags = this.editTags.bind(this);
   }
-  this.itemToAdd("");
-}.bind(this);
-};
 
-ko.applyBindings(new SimpleListModel(["Alpha", "Beta", "Gamma"]));
+  remove(val) {
+      this.items.splice(this.items.indexOf(val), 1);
+      this.updateTagsList();
+  };
+
+  updateTagsList() {
+      this.tagsList(this.items().join(','));
+  };
+
+  isNumber(value){
+      if(!isNaN(value) && value !== '') return true;
+      return false;
+  };
+
+  dataNormalizer(values){
+      let normalizedValues = values.replace(/;|\r?\n|\r/g, ",").split(',');
+      const ret = [];
+      for(let i=0; normalizedValues.length > i; i++){
+          if(this.isNumber(normalizedValues[i]))
+              ret.push(normalizedValues[i]);
+      }
+      return ret;
+  };
+
+  mergeArrayDouplicatePreventer(data1, data2){
+      return [...new Set(data1.concat(data2))]
+  };
+
+  addItem() {
+      let value = this.itemToAdd();
+      let normalizedValues = this.dataNormalizer(value);
+      let MergedCleanedData = this.mergeArrayDouplicatePreventer(this.items(), normalizedValues)
+      this.items(MergedCleanedData);
+      this.itemToAdd("");
+      this.updateTagsList();
+  }
+
+  editTags() {
+      const newValues = this.dataNormalizer(this.tagsList());
+      let MergedCleanedData = [...new Set(newValues)]
+      this.items(MergedCleanedData);
+  };
+}
+
+let TagInputRef = new TagInput(["1", "2", "3"])
+ko.applyBindings(TagInputRef);
+
+
+$('#tagEditor').on('hidden.bs.modal', function () {
+    //update tags in modal's input
+    TagInputRef.updateTagsList()
+})
